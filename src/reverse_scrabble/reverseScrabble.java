61f3a94b814f3af5270baa191cb04f8e635d7a11
CptCup <jack.may.12@cnu.edu>
2017-08-16 16:35:11 -0400

    made method to populate an arraylist of words

--- src/reverse_scrabble/reverseScrabble.java
+++ src/reverse_scrabble/reverseScrabble.java
@@ -3,13 +3,27 @@ package reverse_scrabble;
 /**
  * Created by Jack on 8/16/2017.
  * finds scrabble score in reverse
- * global variable:
- * list diction, word dictionary to be used
+ * global variable: list diction, word dictionary to be used
  */
-public class reverseScrabble {
-    char [][] board;
+import java.util.*;
 
-    public char[][] makeBoard(int dimX, int dimY, char [][] in){
+public class reverseScrabble {
+    ArrayList<String> diction = new ArrayList<String>();
+    Scanner scanWords = new Scanner("enable1.txt");
+    /*
+        method to populate our dictionary of scrabble words
+     */
+    public ArrayList<String> populateScanwords(){
+        while (scanWords.hasNextLine()){
+            diction.add(scanWords.nextLine());
+        }
+        scanWords.close();
+        return diction;
+    }
+    /*
+        Finds all of the words in the board
+     */
+    public ArrayList<String> getWords(char[][] in){
 
     }
 
